09fc5b9ca9c420b7decc92d0af1ff827548af5f9
Ahmad Ilham <ahmad.ilham@amartha.com>
2024-07-01 15:04:38 +0700

    setup vercel speedInsight & analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -21,6 +23,8 @@ export default function RootLayout({ children }) {
         fontSans.variable
       )}>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
